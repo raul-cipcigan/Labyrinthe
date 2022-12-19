4e5789c87db077a29981bf38eaf2b294d9c34c1e
Raul Cipcigan¸ <raul.cipcigan@gmail.com>
2022-12-18 20:11:33 -0500

    Ajout d'un chronomètre

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,40 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
+	public int level;
+	public float levelTime;
+	public TextMeshProUGUI timer;
+
 	private Rigidbody rb;
-	private int level;
 	private float time;
-	private bool playerControl;
+	private float timerStart;
+	private float timeElapsed;
+	private float timeRemaining;
+	private int minutes;
+	private string timerString;
+	private bool playerControl = true;
 	private bool finished = false;
+	private bool dead = false;
+	private bool firstFrame = false;
 
 	private void Start() {
 		rb = GetComponent<Rigidbody>();
-		playerControl = true;
+
+		//Obtient le numéro du niveau
 		level = SceneManager.GetActiveScene().buildIndex;
+
+		timerStart = Time.time;
+
+		Debug.Log(SceneManager.sceneCount);
 	}
 
-    private void Update() {
+	private void Update() {
 		//TODO: Maybe create a save system (but prolly not)
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			Application.Quit();
-        }
-
-		if (SceneManager.sceneCount == 2) {
-
-			//Afin de s'assurer qu'il n'y a pas trop d'objets dans le jeu en même temps
-			SceneManager.UnloadSceneAsync(level - 1);
-
 		}
 
-		if (finished) {
-			NextLevelTimer();
-        }
-    }
+		UpdateTimer();
 
-    private void FixedUpdate() {
+		//La fonction elle-même vérifie à seulement un instant, donc elle est insérée dans Update() pour être répétée à chaque frame
+		if (finished) {
+			SceneLoadTimer(time);
+        } else if (dead) {
+			SceneLoadTimer(time);
+        }
+
+	}
+
+	private void FixedUpdate() {
 		float xAxis = Input.GetAxis("Horizontal");
 		float yAxis = Input.GetAxis("Vertical");
 
@@ -45,24 +60,52 @@ public class PlayerController : MonoBehaviour {
 			rb.AddForce(mov * 10);
 		}
 	}
-    private void OnTriggerEnter(Collider other) {
-        
+
+	private void OnTriggerEnter(Collider other) {
+		
 		//Si le joueur touch un objet "Finish", le prochain niveau est ouvert
 		if (other.gameObject.CompareTag("Finish")) {
 			playerControl = false;
-			level++;
+			//S'assurer qu'un joueur qui perd ne peut pas rentrer dans le but
+			if (!dead) {
+				level++;
+			}
 			time = Time.time;
 			finished = true;
 		}
-    }
+	}
 	
-	private void NextLevelTimer() {
+	private void SceneLoadTimer(float sceneTimer) {
 
 		//Attend soit 5 secondes, ou jusqu'à ce que le joueur appuye un bouton
-		if ((Time.time - time < 5.0f && Time.time - time > 0.05f && Input.anyKeyDown) || (Time.time - time >= 5.0f)) {
+		if ((Time.time - sceneTimer < 5.0f && Time.time - sceneTimer > 0.05f && Input.anyKeyDown) || (Time.time - sceneTimer >= 5.0f)) {
 
-			SceneManager.LoadScene(level);
-       
+			SceneManager.LoadSceneAsync(level);
+			
 		} 
-    }
+	}
+
+	private void UpdateTimer() {
+
+		if (!finished && (timeElapsed.CompareTo(levelTime) < 0)) {
+			timeElapsed = Time.time - timerStart;
+
+			//J'arrondis pour avoir seulement 2 points après la décimale
+			timeElapsed = Mathf.Floor(timeElapsed * 100) / 100;
+
+			timeRemaining = levelTime - timeElapsed;
+			minutes = (int)timeRemaining / 60;
+
+			//J'utilise la variable firstFrame afin de m'assurer que la variable time est seulement prise une fois, et qu'elle n'est pas mise à jour constamment
+		} else if (!firstFrame) {
+			dead = true;
+			playerControl = false;
+			firstFrame = true;
+			time = Time.time;
+		}
+
+		timerString = string.Format("{0}:{1:00.00}", minutes, timeRemaining % 60);
+		timer.text = timerString;
+		
+	}
 }
